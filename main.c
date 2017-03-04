c18e2c1ebd85af641e862b428b35410845ad2fbf
SlackBat <dvtlion@gmail.com>
2017-03-04 12:04:58 +0700

    Main function

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+int allcash(cash, time){
+	if 
+}
+
 int main()
 {
 
+	int cashin, time, cashout, p;
+	
+	printf("How much of cash are you ready to throw: ");
+	scanf("  %d\n", &cashin);
+	
+	printf("How long your money won't be with you: ");
+	scanf("  %d\n", &time);
+	
+	p = (allcash(cashin, time)/365)/100;
+	cashout = cashin + p;
 }
