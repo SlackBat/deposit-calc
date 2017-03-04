f3564879665e4a6a7cae9edb0d18cedb207a6e6f
SlackBat <dvtlion@gmail.com>
2017-03-04 12:30:30 +0700

    Allcash function

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <cmath>
 
 int allcash(cash, time){
-	if 
+	int p;
+	if(time > -1 && time < 31)
+		p = -10;
+	if(time > 30 && time < 121)
+		p = 2;
+	if(time > 120 && time < 241)
+		p = 6;
+	if(time > 240 && time < 366)
+		p = 12;
+		
+	if (cash > 100000)
+		p += floor(p/6)+1;
+		
+	return(cash * time * p);
 }
 
 int main()
